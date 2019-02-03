861f15c5e8b3fee462421f74a767fdd35e2b10f1
Carlos Rodriguez <32423618+CMario210@users.noreply.github.com>
2019-02-03 18:10:51 -0500

    Cosmic version 0.1
    Branch of the original master one, with some quirks added to the AI.
    Still a basic python program :)

---
+++ main.py
@@ -0,0 +1,173 @@
+#Escrito por Carlos Rodriguez
+#CC:193493462   CU:201711285186
+
+import random
+
+nombreJugador = ''
+
+def imprimirTabla(tabla):
+    print('\
+ ╔═══╦═══╦═══╗\n\
+ ║ {0} ║ {1} ║ {2} ║\n\
+ ╠═══╬═══╬═══╣\n\
+ ║ {3} ║ {4} ║ {5} ║\n\
+ ╠═══╬═══╬═══╣\n\
+ ║ {6} ║ {7} ║ {8} ║\n\
+ ╚═══╩═══╩═══╝ '.format(
+               tabla[7], tabla[8], tabla[9],
+               tabla[4], tabla[5], tabla[6],
+               tabla[1], tabla[2], tabla[3]))
+
+
+def letraJugador():
+    repetir = True
+    while repetir:
+        print("Juegas con X ó O?")
+        letra = input()
+        if letra == "x" or letra == "X" or letra == "o" or letra == "O":
+            repetir = False
+            if letra == "x" or letra == "X":
+                return 'X'
+            else:
+                return 'O'
+        else:
+            print(nombreJugador + ", " + letra + " no es un caracter valido!")
+
+
+def primero():
+    if random.randint(0, 1) == 0:
+        return 'AL-8999'
+    else:
+        return nombreJugador
+
+
+def volverJuego():
+    repetir = True
+    while repetir:
+        print("¿Quieres volver a jugar Si o No?")
+        opcion = input()
+        if opcion == "si" or opcion == "Si" or opcion == "SI" or opcion == "s" or opcion == "S":
+            return False
+            repetir = False
+        else:
+            if opcion == "no" or opcion == "No" or opcion == "NO" or opcion == "n" or opcion == "N":
+                return True
+                repetir = False
+            else:
+                print(nombreJugador + " creo que eso no se puede hacer...")
+
+
+def colocar(tabla, jugador, movimiento):
+    tabla[movimiento] = jugador
+
+
+def ganar(tabla, Jugador):
+    if ((tabla[7] == Jugador and tabla[4] == Jugador and tabla[1] == Jugador) or
+            (tabla[8] == Jugador and tabla[5] == Jugador and tabla[2] == Jugador) or
+            (tabla[9] == Jugador and tabla[6] == Jugador and tabla[3] == Jugador) or
+            (tabla[7] == Jugador and tabla[8] == Jugador and tabla[9] == Jugador) or
+            (tabla[4] == Jugador and tabla[5] == Jugador and tabla[6] == Jugador) or
+            (tabla[1] == Jugador and tabla[2] == Jugador and tabla[3] == Jugador) or
+            (tabla[7] == Jugador and tabla[5] == Jugador and tabla[3] == Jugador) or
+            (tabla[9] == Jugador and tabla[5] == Jugador and tabla[1] == Jugador)):
+        return True
+
+
+def copiarTabla(tabla):
+    tabla2 = []
+    for i in tabla:
+        tabla2.append(i)
+    return tabla2
+
+
+def espacioVacio(tabla, movimiento):
+    if tabla[movimiento] == ' ':
+        return True
+
+
+def movimientoHumano(tabla):
+    repetir = True
+    while repetir:
+        movimiento = input()
+        if movimiento == '1' or movimiento == '2' or movimiento == '3' or movimiento == '4' or movimiento == '5' or movimiento == '6' or movimiento == '7' or movimiento == '8' or movimiento == '9':
+            if espacioVacio(tabla, int(movimiento)):
+                repetir = False
+                return int(movimiento)
+            else:
+                print("Ese espacio está ocupado " + nombreJugador.lower())
+        else:
+            print("Eso ni siquiera es un caracter, aprende a escribir primate")
+
+
+def movimientoComputador(tabla, computador, humano):
+    for i in range(1, 10):
+        tablaTemporal = copiarTabla(tabla)
+        if espacioVacio(tablaTemporal, i):
+            colocar(tablaTemporal, computador, i)
+            if ganar(tablaTemporal, computador):
+                return i
+    for i in range(1, 10):
+        tablaTemporal = copiarTabla(tabla)
+        if espacioVacio(tablaTemporal, i):
+            colocar(tablaTemporal, humano, i)
+            if ganar(tablaTemporal, humano):
+                return i
+    posiblesMovimientos = [] * 10
+    for i in range(1, 10):
+        if espacioVacio(tabla, i):
+            posiblesMovimientos.append(i)
+    return random.choice(posiblesMovimientos)
+
+
+def tablaLlena(tabla):
+    for i in range(1, 10):
+        if espacioVacio(tabla, i):
+            return False
+    return True
+
+
+while True:
+    tabla = [' '] * 10
+    if nombreJugador == '':
+        nombreJugador = input("Cómo te llamas?\n")
+    humano = letraJugador()
+    if humano == 'X':
+        computador = 'O'
+    else:
+        computador = 'X'
+    turno = primero()
+    print(turno + ' comienza el turno')
+    juegoCorre = True
+    while juegoCorre:
+        if turno == nombreJugador:
+            imprimirTabla(tabla)
+            movimiento = movimientoHumano(tabla)
+            colocar(tabla, humano, movimiento)
+            if ganar(tabla, humano):
+                imprimirTabla(tabla)
+                print('Valla, esto si es una novedad, has ganado ' + nombreJugador.lower())
+                juegoCorre = False
+            else:
+                if tablaLlena(tabla):
+                    imprimirTabla(tabla)
+                    print('Empate!\nSorprendente para ser un descendiente del mono')
+                    juegoCorre = False
+                else:
+                    turno = 'computador'
+        else:
+            movimiento = movimientoComputador(tabla, computador, humano)
+            colocar(tabla, computador, movimiento)
+            if ganar(tabla, computador):
+                imprimirTabla(tabla)
+                print('Has Perdido, eres patetico '+ nombreJugador.lower() +' >:)')
+                juegoCorre = False
+            else:
+                if tablaLlena(tabla):
+                    imprimirTabla(tabla)
+                    print('Empate!\nHonores ' + nombreJugador.lower())
+                    juegoCorre = False
+                else:
+                    turno = nombreJugador
+    if volverJuego():
+        print('Esto fue muy divertido ' + nombreJugador.lower() + ' nwn')
+        exit()
